daba98db018de2a6da0f129648b2fea1f0c934f0
JeanMB2003 <95185531+JeanMB2003@users.noreply.github.com>
2024-08-13 21:14:28 -0300

    Update sketch.js
    new

--- sketch.js
+++ sketch.js
@@ -1,15 +1,12 @@
 function preload() {
-  objeto = loadModel('tinker.obj');
   img = loadImage('Captura de Tela (1).png')
 }
 
 function setup() {
-  createCanvas(400, 400, WEBGL);
+  createCanvas(1000, 1000);
 }
 
 function draw() {
-  background(220);
-  model(objeto);
-  orbitControl();
+  background('blue');
   image(img, 100,100)
 }
